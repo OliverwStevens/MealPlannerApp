56a9391c1286e9c9e72a197c4077beff772c102e
Oliver Stevens <oliverwstevens@gmail.com>
2025-04-22 11:55:31 -0400

    Switched to modern approach and downloaded zxing library

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "../stylesheets/application.scss"
-import "@zxing/library";
+//import "../stylesheets/application.scss"
+//import "@zxing/library";
+import * as ZXing from "@zxing/library"
+console.log("MINIMAL TEST - This should appear")
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM fully loaded")
+})
+
+document.addEventListener("turbo:load", () => {
+  console.log("Turbo loaded a page")
+})
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,11 @@
 # Pin npm packages by running ./bin/importmap
-
-pin "application"
+# config/importmap.rb
+pin "application", preload: true
+pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
-pin "@zxing/library", to: "@zxing--library.js" # @0.21.3
+# config/importmap.rb
+# pin "@zxing/library", to: "https://unpkg.com/@zxing/library@0.21.3/umd/index.min.js"
+pin "@zxing/library", to: "zxing-umd.min.js", preload: true
 pin "ts-custom-error" # @3.3.1
