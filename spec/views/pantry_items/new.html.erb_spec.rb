df2c8f5e7114c017777d4af3e9dba142e9f411f7
Oliver Stevens <oliverwstevens@gmail.com>
2025-04-28 13:06:19 -0400

    fixed views/pantry_items/new rspec test

--- spec/views/pantry_items/new.html.erb_spec.rb
+++ spec/views/pantry_items/new.html.erb_spec.rb
@@ -1,14 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe "pantry_items/new", type: :view do
-  before(:each) do
-    assign(:pantry_item, PantryItem.new())
+  let(:user) { create(:user) }
+
+  before do
+    allow(view).to receive(:current_user).and_return(user)
+    allow(view).to receive(:user_signed_in?).and_return(true)
+
+    assign(:pantry_item, PantryItem.new)
   end
 
   it "renders new pantry_item form" do
     render
 
-    assert_select "form[action=?][method=?]", pantry_items_path, "post" do
-    end
+    # Basic assertions
+    expect(rendered).to have_selector("form[action='#{pantry_items_path}'][method='post']")
+    expect(rendered).to have_selector("input[type='hidden'][name='pantry_item[name]']", visible: false)
+    expect(rendered).to have_selector("input[type='hidden'][name='pantry_item[barcode]'][id='barcode-result']", visible: false)
+    expect(rendered).to have_selector("input[type='hidden'][name='pantry_item[user_id]'][value='#{user.id}']", visible: false)
+
+    expect(rendered).to have_selector("video#scanner-video")
+
+    expect(rendered).to have_selector("input[type='submit'][class='hidden-submit']", visible: false)
   end
 end
