56cfd3ee16e2de3e661babd8ebb0b91f25bf67f6
Oliver Stevens <oliverwstevens@gmail.com>
2025-04-28 13:18:08 -0400

    Fixed views/pantry_items/show rspec test

--- spec/views/pantry_items/show.html.erb_spec.rb
+++ spec/views/pantry_items/show.html.erb_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "pantry_items/show", type: :view do
-  before(:each) do
-    assign(:pantry_item, PantryItem.create!())
+  let(:user) { create(:user) }
+  let(:pantry_item) { create(:pantry_item, user: user) }
+
+  before do
+    allow(view).to receive(:current_user).and_return(user)
+    allow(view).to receive(:user_signed_in?).and_return(true)
+
+    assign(:pantry_item, pantry_item)
   end
 
-  it "renders attributes in <p>" do
+  it "renders attributes" do
     render
+    expect(rendered).to include(pantry_item.name)
+    expect(rendered).to include(pantry_item.barcode)
   end
 end
