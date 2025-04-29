8ba4f5ebd3c724d2b87096dad2c548c566ef0948
Oliver Stevens <oliverwstevens@gmail.com>
2025-04-29 13:15:53 -0400

    Worked on formating images correctly and fixed barcode scan reference

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-//import "barcode_scanner"
+import "barcode_scanner"
 //import "../stylesheets/application.scss"
 //import "@zxing/library";
 import * as ZXing from "@zxing/library"
