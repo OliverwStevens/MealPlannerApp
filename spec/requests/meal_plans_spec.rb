d42d46567038fc5b5b5c35ad98afa73351cde942
Oliver Stevens <oliverwstevens@gmail.com>
2025-05-05 14:38:35 -0400

    Fixed meal plans test

--- spec/requests/meal_plans_spec.rb
+++ spec/requests/meal_plans_spec.rb
@@ -1,32 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "MealPlans", type: :request do
+  let(:user) { create(:user) }
+
+  before do
+    sign_in user
+  end
   describe "GET /index" do
     it "returns http success" do
-      get "/meal_plans/index"
+      get "/meal_plans"
       expect(response).to have_http_status(:success)
     end
   end
-
-  describe "GET /new" do
-    it "returns http success" do
-      get "/meal_plans/new"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /create" do
-    it "returns http success" do
-      get "/meal_plans/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/meal_plans/destroy"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
