4fa08608c3406ac34c022c3798eb83bb0e04aa26
Oliver Stevens <oliverwstevens@gmail.com>
2025-05-09 21:11:56 -0400

    Wrote views/meals/index test

--- spec/views/meals/index.html.erb_spec.rb
+++ spec/views/meals/index.html.erb_spec.rb
@@ -1,26 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe "meals/index", type: :view do
-  before(:each) do
-    assign(:meals, [
-      Meal.create!(
-        name: "Name",
-        description: "MyText",
-        user: nil
-      ),
-      Meal.create!(
-        name: "Name",
-        description: "MyText",
-        user: nil
-      )
-    ])
+  let(:user) { create(:user) }
+  let!(:meals) { create_list(:meal, 2, user: user) }
+
+  before do
+    # Create a paginated collection that responds to pagination methods
+    paginated_meals = Kaminari.paginate_array(meals).page(1).per(10)
+    assign(:meals, paginated_meals)
+    assign(:total_meals_count, 2) # Match the number of meals you created
+    allow(view).to receive(:current_user).and_return(user)
   end
 
-  it "renders a list of meals" do
+  it "renders the meals index page correctly" do
     render
-    cell_selector = 'div>p'
-    assert_select cell_selector, text: Regexp.new("Name".to_s), count: 2
-    assert_select cell_selector, text: Regexp.new("MyText".to_s), count: 2
-    assert_select cell_selector, text: Regexp.new(nil.to_s), count: 2
+
+    # Test the title and header
+    expect(rendered).to include("<h1>Meals</h1>")
+    expect(rendered).to include("You have 2 meal(s)")
+
+    # Test the new meal button
+    assert_select "a.floating-green-button[href=?]", new_meal_path
+    assert_select "img.floating-button-icon[alt='Add meal']"
+
+    # Test meal listings
+    assert_select "#meals.food-display" do
+      assert_select "a.centered-text.hover-move", count: 2
+      meals.each do |meal|
+        assert_select "a[href=?]", meal_path(meal)
+      end
+    end
+
+    # Test infinite scroll container
+    assert_select "div[data-controller='infinite-scroll']" do
+      assert_select "div[data-infinite-scroll-target='entries']"
+    end
   end
 end
