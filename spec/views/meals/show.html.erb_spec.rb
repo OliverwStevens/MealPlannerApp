fddc13f8f8fa7a7165afb255fed54e93fcace69e
Oliver Stevens <oliverwstevens@gmail.com>
2025-05-10 08:58:03 -0400

    Fixed views/meals/show test

--- spec/views/meals/show.html.erb_spec.rb
+++ spec/views/meals/show.html.erb_spec.rb
@@ -1,18 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe "meals/show", type: :view do
+  let(:user) { build(:user) }
+  let(:meal) { create(:meal) }
   before(:each) do
-    assign(:meal, Meal.create!(
-      name: "Name",
-      description: "MyText",
-      user: nil
-    ))
+    allow(view).to receive(:user_signed_in?).and_return(true)
+    allow(view).to receive(:current_user).and_return(user)
+
+    assign(:meal, meal)
   end
 
   it "renders attributes in <p>" do
     render
-    expect(rendered).to match(/Name/)
-    expect(rendered).to match(/MyText/)
-    expect(rendered).to match(//)
+    expect(rendered).to include(meal.name)
+
+    expect(rendered).to include(meal.description)
+
+    expect(rendered).to include(meal.meal_type)
+    # Not checking for sharable, not sure if I will. UI kinda looks nice without it
+
+    meal.recipes.each do |recipe|
+      expect(rendered).to include(recipe.name)
+    end
   end
 end
