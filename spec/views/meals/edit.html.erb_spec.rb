9889b20aebcf71d479d7d321ef277ed6a0a60483
Oliver Stevens <oliverwstevens@gmail.com>
2025-05-09 21:02:51 -0400

    Fixed views/meals/edit

--- spec/views/meals/edit.html.erb_spec.rb
+++ spec/views/meals/edit.html.erb_spec.rb
@@ -1,28 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "meals/edit", type: :view do
-  let(:meal) {
-    Meal.create!(
-      name: "MyString",
-      description: "MyText",
-      user: nil
-    )
-  }
+  let(:user) { create(:user) }
+  let(:meal) { create(:meal, user: user) }  # Associate meal with user
 
-  before(:each) do
+  before do
+    # Stub current_user for the view
+    allow(view).to receive(:current_user).and_return(user)
     assign(:meal, meal)
   end
 
   it "renders the edit meal form" do
     render
-
     assert_select "form[action=?][method=?]", meal_path(meal), "post" do
-
       assert_select "input[name=?]", "meal[name]"
-
       assert_select "textarea[name=?]", "meal[description]"
-
-      assert_select "input[name=?]", "meal[user_id]"
     end
   end
 end
