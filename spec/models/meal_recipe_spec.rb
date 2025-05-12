aabce227925163a5dcf4eb3ddda43cb34be92084
Oliver Stevens <oliverwstevens@gmail.com>
2025-05-12 09:53:16 -0400

    Wrote tests for meal recipe model

--- spec/models/meal_recipe_spec.rb
+++ spec/models/meal_recipe_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe MealRecipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:meal) }
+    it { should belong_to(:recipe) }
+  end
+
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      meal_recipe = build(:meal_recipe)
+      expect(meal_recipe).to be_valid
+    end
+
+    it 'is invalid without a meal' do
+      meal_recipe = build(:meal_recipe, meal: nil)
+      expect(meal_recipe).not_to be_valid
+      expect(meal_recipe.errors[:meal]).to include("must exist")
+    end
+
+    it 'is invalid without a recipe' do
+      meal_recipe = build(:meal_recipe, recipe: nil)
+      expect(meal_recipe).not_to be_valid
+      expect(meal_recipe.errors[:recipe]).to include("must exist")
+    end
+  end
 end
